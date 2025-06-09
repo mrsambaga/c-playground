6803d5a3a5dc77cb532dd34fea11d3033fc9fb6b
mrsambaga <m.r.sambaga@gmail.com>
2025-06-09 09:04:15 +0700

    refactor input function to separate function

--- Project1/main.c
+++ Project1/main.c
@@ -5,16 +5,18 @@ void printName(const char *name)
     printf("Hello my name is %s\n", name);
 }
 
+void printAndInputNumber() 
+{
+    int myFavoriteNumber = 0;
+    printf("Please enter your favorite number\n");
+	scanf("%d", &myFavoriteNumber);
+    printf("You entered number : %d\n", myFavoriteNumber);
+}
 
 int main(int argc, char **argv)
 {
     printName("Sam");
-    
-	int myFavoriteNumber = 0;
-    
-    printf("Please enter your favorite number\n");
-	scanf("%d", &myFavoriteNumber);
-    printf("You entered number : %d\n", myFavoriteNumber);
-    
+    printAndInputNumber();
+        
     return 0;
 }
