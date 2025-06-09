68fc7f19463921e3f51dbc04dc91b60c8778afa3
mrsambaga <m.r.sambaga@gmail.com>
2025-06-09 09:02:43 +0700

    add function to print string

--- Project1/main.c
+++ Project1/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+void printName(const char *name)
+{
+    printf("Hello my name is %s\n", name);
+}
+
+
 int main(int argc, char **argv)
 {
+    printName("Sam");
+    
 	int myFavoriteNumber = 0;
     
     printf("Please enter your favorite number\n");
