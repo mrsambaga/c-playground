0861faec072168b35b911f9a55451deb3198cee6
mrsambaga <m.r.sambaga@gmail.com>
2025-06-10 19:42:15 +0700

    add codes for c data types

--- Project1/main.c
+++ Project1/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void printName(const char *name)
 {
@@ -11,6 +12,15 @@ void printAndInputNumber()
     printf("Please enter your favorite number\n");
 	scanf("%d", &myFavoriteNumber);
     printf("You entered number : %d\n", myFavoriteNumber);
+}
+
+void dataTypes()
+{
+    int integerData = 1;
+    float floatData = 5.33;
+    bool boolData = true;
+    long longData = 100000000000000;
+    double doubleData = 5.333333333333333333;
 }
 
 int main(int argc, char **argv)
