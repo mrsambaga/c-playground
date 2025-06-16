aae53d269c78a82e67089570968ea57f25be628e
mrsambaga <m.r.sambaga@gmail.com>
2025-06-16 20:20:39 +0700

    add code for calculating rainfall challenge

--- Project1/main.c
+++ Project1/main.c
@@ -260,6 +260,50 @@ void findPrimeNumbers()
     }
 }
 
+//Calculate Average Rainfall Challenge
+void calculateRainfall()
+{
+    int years[5] = {2010, 2011, 2012, 2013, 2014};
+    float rainfalls[5][12] = {
+        {2.1, 4.3, 1.1, 3.4, 0.8, 2.7, 4.9, 1.5, 3.8, 0.2, 2.5, 4.6},
+        {0.9, 3.1, 4.8, 2.2, 1.7, 0.6, 3.9, 4.2, 1.3, 2.8, 0.7, 3.5},
+        {4.4, 1.9, 0.3, 3.6, 2.0, 4.1, 1.2, 3.3, 0.5, 4.7, 2.4, 1.0},
+        {3.2, 0.1, 2.6, 4.0, 1.4, 3.0, 2.3, 0.0, 4.5, 1.8, 3.7, 2.9},
+        {1.6, 4.2, 3.5, 0.7, 2.9, 1.2, 4.6, 2.1, 0.4, 3.3, 1.9, 4.8}
+    };
+    float totalYearlyRainfalls[5] = {0};
+    float sumRainfalls = 0.0;
+    
+    printf("YEAR RAINFALL\n");
+            
+    for (int i = 0; i < 5; i++) {
+        for (int l = 0; l < 12; l++) {
+            totalYearlyRainfalls[i] += rainfalls[i][l];
+        }
+        
+        sumRainfalls += totalYearlyRainfalls[i];
+        printf("%d %.1f\n", years[i], totalYearlyRainfalls[i]);
+    }
+        
+    printf("The yearly average is %.1f inches.\n", sumRainfalls / 5);
+    
+    
+    float totalMonthlyRainfalls[12] = {0};
+
+    printf("MONTHLY AVERAGES: \n");
+    printf("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec\n");
+    
+    for (int i = 0; i < 12; i++) {
+        for (int l = 0; l < 5; l++) {
+            totalMonthlyRainfalls[i] += rainfalls[l][i];
+        }
+        
+        printf("%.1f ", totalMonthlyRainfalls[i] / 5);
+    }
+    
+    printf("\n");
+}
+
 int main(int argc, char **argv)
 {
 //    printName("Sam");
@@ -274,7 +318,8 @@ int main(int argc, char **argv)
 //    switchStatement();
 //    guessNumber();
 //    arrays();
-    findPrimeNumbers();
+//    findPrimeNumbers();
+    calculateRainfall();
         
     return 0;
 }
