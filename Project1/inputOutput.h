1504e39fdd0d1627bfa34194232f513e7e280718
mrsambaga <m.r.sambaga@gmail.com>
2025-06-18 23:07:18 +0700

    refactor input output to separate file

---
+++ Project1/inputOutput.h
@@ -0,0 +1,9 @@
+#ifndef INPUTOUTPUT_H
+#define INPUTOUTPUT_H
+
+#include <stdbool.h>
+
+void printName(const char *name);
+void printAndInputNumber();
+
+#endif
--- Project1/main.c
+++ Project1/main.c
@@ -6,19 +6,6 @@
 #include "functions.h"
 #include "tictactoe.h"
 
-void printName(const char *name)
-{
-    printf("Hello my name is %s\n", name);
-}
-
-void printAndInputNumber() 
-{
-    int myFavoriteNumber = 0;
-    printf("Please enter your favorite number\n");
-	scanf("%d", &myFavoriteNumber);
-    printf("You entered number : %d\n", myFavoriteNumber);
-}
-
 void dataTypes()
 {
     //Basic
