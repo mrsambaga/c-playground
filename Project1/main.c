78485819e8c8f653208125568431ac35f2382a50
mrsambaga <m.r.sambaga@gmail.com>
2025-07-01 21:53:44 +0700

    create code for malloc challenge

--- Project1/main.c
+++ Project1/main.c
@@ -107,12 +107,29 @@ void dynamicMemoryAllocation()
     str = (char *) realloc(str, 25);
     strcpy(str, "Hello World");
     printf("String: %s, Address: %p\n", str, str);
-    
+        
     free(str);
+}
+
+void malocChallenge()
+{
+    int size;
+    char *text = NULL;
+    printf("Please enter a memory size: ");
+    scanf("%d", &size);
+    
+    text = (char *)malloc(size * sizeof(char));
+    
+    printf("Please enter a text: ");
+    scanf("%s", text);
+    
+    printf("Your text: %s\n", text);
+    
+    free(text);
 }
 
 int main(int argc, char **argv)
 {
-    dynamicMemoryAllocation();
+    malocChallenge();
     return 0;
 }
