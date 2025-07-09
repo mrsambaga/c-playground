51aa347a4340ac21f48f67d04763a8288b1979bc
mrsambaga <m.r.sambaga@gmail.com>
2025-07-09 20:12:17 +0700

    add code for reverse file content

--- c-tutorial/main.c
+++ c-tutorial/main.c
@@ -167,8 +167,35 @@ void filePositioning()
     return;
 }
 
+void reverseFileContent()
+{
+    FILE *fp = NULL;
+    int len;
+    int i = 0;
+    
+    fp = fopen("file.txt", "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    
+    fseek(fp, 0, SEEK_END);
+    len = ftell(fp);
+    
+    while (i < len) {
+        i++;
+        fseek(fp, -i, SEEK_END);
+        printf("%c", fgetc(fp));
+    }
+    printf("\n");
+    
+    fclose(fp);
+    
+    return;
+}
+
 int main(int argc, char **argv)
 {
-    filePositioning();
+    reverseFileContent();
     return 0;
 }
