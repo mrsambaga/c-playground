8b4f0fa248604be2322b13c79022b7623edac93d
mrsambaga <m.r.sambaga@gmail.com>
2025-07-09 20:22:32 +0700

    refactor file input output code into separate file

---
+++ c-tutorial/file.h
@@ -0,0 +1,11 @@
+#ifndef POINTER_H
+#define POINTER_H
+
+void fileInputOutput();
+void writeFile();
+void countFileRow();
+void toUpperCase();
+void filePositioning();
+void reverseFileContent();
+
+#endif
