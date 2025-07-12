c51a0cc4384e301d53c2a0ca1051e6e16c24be0f
mrsambaga <m.r.sambaga@gmail.com>
2025-07-12 14:12:03 +0700

    refactor input output logic

--- command-line-shell/main.c
+++ command-line-shell/main.c
@@ -10,7 +10,17 @@ int main(int argc, char **argv)
     
     while (1)
     {
-        scanf("%s", input);
+        printf("$ ");
+        fflush(stdout);
+        
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+        
+        input[strcspn(input, "\n")] = '\0';
+        
+        printf("You entered: %s\n", input);
         
         if (strcmp(input, "end") == 0){
             break;
