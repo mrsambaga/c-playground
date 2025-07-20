be6cd7c42d4fe41afc4c03d7cd7e1b5c670ff288
mrsambaga <m.r.sambaga@gmail.com>
2025-07-20 22:13:13 +0700

    change network datatype

--- network-programming/main.c
+++ network-programming/main.c
@@ -6,9 +6,9 @@ void networkCovertion()
     uint16_t port = 8080;
     uint16_t net_port = htons(port);
     
-    printf("Original port number : %d\n", port);
-    printf("Host to network byte convertion : %d\n", net_port);
-    printf("Network byte to host convertion : %d\n", ntohs(net_port));
+    printf("Original port number : %u\n", port);
+    printf("Host to network byte convertion : 0x%04X\n", net_port);
+    printf("Network byte to host convertion : %u\n", ntohs(net_port));
     return;
 }
 
