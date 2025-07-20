396168d0ba66409aeb80c666bda24a9af4167654
mrsambaga <m.r.sambaga@gmail.com>
2025-07-20 22:12:17 +0700

    create code for network convertion

--- network-programming/main.c
+++ network-programming/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <netinet/in.h>
+
+void networkCovertion()
+{
+    uint16_t port = 8080;
+    uint16_t net_port = htons(port);
+    
+    printf("Original port number : %d\n", port);
+    printf("Host to network byte convertion : %d\n", net_port);
+    printf("Network byte to host convertion : %d\n", ntohs(net_port));
+    return;
+}
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	networkCovertion();
 	return 0;
 }
