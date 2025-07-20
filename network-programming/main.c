88ae9c161931054052eada082d6bb485f3a59958
mrsambaga <m.r.sambaga@gmail.com>
2025-07-20 22:18:34 +0700

    add code for ip address convertion

--- network-programming/main.c
+++ network-programming/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 void networkCovertion()
 {
@@ -9,6 +10,27 @@ void networkCovertion()
     printf("Original port number : %u\n", port);
     printf("Host to network byte convertion : 0x%04X\n", net_port);
     printf("Network byte to host convertion : %u\n", ntohs(net_port));
+    return;
+}
+
+void ipAddressConvertion()
+{
+    const char *ip_str = "192.168.1.1";
+    struct in_addr addr;
+    
+    addr.s_addr = inet_addr(ip_str);
+    
+    
+    if (addr.s_addr == INADDR_NONE) {
+        printf("Invalid IP: %s\n", ip_str);
+    } else {
+        printf("IP string: %s\n", ip_str);
+        printf("Network-ordered binary (hex): 0x%08X\n", addr.s_addr);
+        
+        char *back_to_str = inet_ntoa(addr);
+        printf("Back to IP string: %s\n", back_to_str);
+    }
+    
     return;
 }
 
