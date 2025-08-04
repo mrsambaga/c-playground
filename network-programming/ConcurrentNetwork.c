91d9f5620dd1d2b6b8abac575cc0e4def02d9384
mrsambaga <m.r.sambaga@gmail.com>
2025-08-04 20:28:08 +0700

    delete unused comment

--- network-programming/ConcurrentNetwork.c
+++ network-programming/ConcurrentNetwork.c
@@ -69,10 +69,3 @@ void concurrentServer()
     
     return;
 }
-
-//private *get_in_addr(struct sockaddr *sa){
-//    if (sa.sa_family == AF_INET) {
-//        return &(((struct sockaddr_in*)sa).sin_addr);
-//    }
-//    return return &(((struct sockaddr_in6*)sa).sin6_addr);
-//}
